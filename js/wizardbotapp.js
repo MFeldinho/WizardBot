2e2fcf8ddddc2ef62836c26065d1a5250bda002b
MFeldinho <m.felder@live.de>
2015-06-27 09:44:32 +0200

    ..

--- js/wizardbotapp.js
+++ js/wizardbotapp.js
@@ -1,15 +1,39 @@
 /// <reference path="../typings/angularjs/angular.d.ts"/>
-var app = angular.module('WizardBotApp', []);
+var app = angular.module('WizardBotApp', ['ngRoute']);
+
+//Define Routing for app
+//Uri /Home -> template home.html and Controller HomeController
+//Uri /About -> template about.html and Controller AboutController
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/Home', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+      }).
+      when('/About', {
+        templateUrl: 'templates/about.html',
+        controller: 'AboutController'
+      }).
+      when('/AllCards', {
+        templateUrl: 'templates/allcards.html',
+        controller: 'AllCardsController'
+      }).
+      otherwise({
+        redirectTo: '/Home'
+      });
+}]);
 
 app.controller('HomeController', function($scope) {
-    $scope.greeting = 'Hello!';
+    $scope.message = 'Home';
 });
 
 app.controller('AboutController', function($scope) {
-    
+    $scope.message = 'About';
 });
 
-app.controller('CardsArchiveController', function($scope) {
+app.controller('AllCardsController', function($scope) {
+    $scope.message = 'All Cards';
     $scope.cards = [
         {id:'r01',name:'Red 1'},
         {id:'r02',name:'Red 2'},
