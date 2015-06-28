1a08a502289ee9f0d43487bc6c3aaf15095d5c8e
MFeldinho <m.felder@live.de>
2015-06-28 20:50:23 +0200

    repo

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,6 @@
+/// <reference path="../typings/angularjs/angular.d.ts"/>
+
+// The main WizardBotApp including the needed controllers starts here
 var wizardBotControllers = angular.module('wizardBotControllers', []);
 
 wizardBotControllers.controller('HomeController', ['$scope', '$routeParams',
@@ -13,9 +16,28 @@ wizardBotControllers.controller('AboutController', ['$scope', '$routeParams',
 wizardBotControllers.controller('AllCardsController', ['$scope', '$routeParams',
   function($scope, $routeParams) {
     $scope.message = 'All Cards';
-    $scope.cards = [
-        {id:'r01',name:'Red 1'},
-        {id:'r02',name:'Red 2'},
-        {id:'w01',name:'Wizard 1'}
-    ];
+    
+    var repo = new WBA.CardRepo(true);
+    $scope.cards = repo.getAllCards();
   }]);
+  
+  
+// Repository starts here
+var WBA = WBA || {};
+
+WBA.CardRepo = function(isMock) {
+  this.isMock = isMock;
+  
+  WBA.CardRepo.prototype.getAllCards = function() {
+    if(this.isMock) {
+      return [
+          { id:'r01', name:'Red 1' },
+          { id:'r02', name:'Red 2' },
+          { id:'w01', name:'Wizard 1' },
+          { id:'g01', name:'Goop 1' }
+      ];
+    }
+    
+    return '';
+  };
+};
